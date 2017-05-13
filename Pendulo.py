f099e0f0dc86d7aac0c621ca3b5a8c83c27b68e1
Munique <muniqueeva@id.uff.br>
2017-05-13 16:57:51 -0300

    XxT VxT

---
+++ Pendulo.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def verlet(xt,vt):
+   p=xt+vt*dt+0.5*((-w)*math.sin(xt)-gama*vt)*dt**2
+   aux=((-w)*math.sin(p)-gama*vt)
+   vaux=vt+0.5*(((-w)*math.sin(xt)-gama*vt)+aux)*dt
+   aux=((-w)*math.sin(p)-gama*vaux)
+   f=vt+0.5*((-w)*math.sin(xt)-gama*vt+aux)*dt
+   return p,f
+
+w=3
+gama=0.5
+xt1=1
+vt1=0
+dt=0.01
+t1=0
+x=[1]
+v=[0]
+t=[0]
+
+while t1<60:
+   t1+=dt
+   xt1,vt1=verlet(xt1,vt1)
+   x.append(xt1)
+   v.append(vt1)
+   t.append(t1)
+   if t1<55:
+      print "x=%f; v=%f; t=%f\n"%(xt1,vt1,t1)
+   
+plt.figure(figsize=(6,5), dpi=96)
+#plt.axis([0,10,-0.2,0.2])
+#plt.xticks(np.linspace(0,10,6,endpoint=True))
+#plt.yticks(np.linspace(-1.5,2,8,endpoint=True))
+
+ax=plt.gca()
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+ax.autoscale()
+
+plt.rc('text', usetex=True)
+plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
+plt.xlabel('Tempo(s)')
+plt.ylabel(r'Posi\c{c}\~{a}o (m) e Velocidade($\frac{m}{s}$)')
+
+plt.title(r'Pendulum Moviment', fontsize=12)
+plt.grid()
+plt.plot(t,x,'r-', linewidth=1, label="$x_{(t)}$")
+plt.plot(t,v,'b-', linewidth=1, label="$v_{(t)}$")
+plt.legend(loc='upper right')
+#plt.savefig("MVE.pdf", dpi=96)
+plt.show()
+#zera em x=0.000001; v=0.000000; t=54.980000
