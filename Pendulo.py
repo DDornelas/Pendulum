bd47c55f9e9018945a752c8a6a3a3ad157f659e0
Munique <muniqueeva@id.uff.br>
2017-05-14 11:30:44 -0300

    Gama sys XxT e VxT

--- Pendulo.py
+++ Pendulo.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import sys
 
 def verlet(xt,vt):
-   p=xt+vt*dt+0.5*((-w)*math.sin(xt)-gama*vt)*dt**2
-   aux=((-w)*math.sin(p)-gama*vt)
-   vaux=vt+0.5*(((-w)*math.sin(xt)-gama*vt)+aux)*dt
-   aux=((-w)*math.sin(p)-gama*vaux)
-   f=vt+0.5*((-w)*math.sin(xt)-gama*vt+aux)*dt
+   p=xt+vt*dt+0.5*((-w)*math.sin(xt)-(eval(sys.argv[1]))*vt)*dt**2
+   aux=((-w)*math.sin(p)-(eval(sys.argv[1]))*vt)
+   vaux=vt+0.5*(((-w)*math.sin(xt)-(eval(sys.argv[1]))*vt)+aux)*dt
+   aux=((-w)*math.sin(p)-(eval(sys.argv[1]))*vaux)
+   f=vt+0.5*((-w)*math.sin(xt)-(eval(sys.argv[1]))*vt+aux)*dt
    return p,f
 
 w=3
-gama=0.5
 xt1=1
 vt1=0
 dt=0.01
@@ -27,21 +27,22 @@ while t1<30:
    v.append(vt1)
    t.append(t1)
    
-   
 plt.figure(figsize=(6,5), dpi=96)
+plt.axis([0,30,-1.5,1.5])
 
 ax=plt.gca()
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-ax.autoscale()
 
 plt.rc('text', usetex=True)
 plt.rc('font', **{'sans-serif' : 'Arial', 'family' : 'sans-serif'})
-plt.xlabel('Posi\c{c}\~{a}o (m)')
-plt.ylabel(r'Velocidade($\frac{m}{s}$)')
+plt.xlabel('Tempo (s)')
+plt.ylabel(r'Posi\c{c}\~{a}o (m) e Velocidade($\frac{m}{s}$)')
 
-plt.title(r'Pendulum Moviment Espa\c{c}o de Fases', fontsize=12)
+plt.title(r'Pendulum Moviment $\gamma$=0,7', fontsize=12)
 plt.grid()
-plt.plot(x,v,'r-', linewidth=1)
-plt.savefig("XxV.pdf", dpi=96)
+plt.plot(t,x,'r-', linewidth=1, label="$x_{(t)}$")
+plt.plot(t,v,'b-', linewidth=1, label="$v_{(t)}$")
+plt.legend(loc='upper right')
+plt.savefig("G07.pdf", dpi=96)
 plt.show()
